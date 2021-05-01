d2b736aaf8ca0bbe24245a47402a2a6b7e5bade8
damour <tuyishimejeand88@gmail.com>
2021-05-01 08:49:12 +0200

    adding menu items

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use std::io;
 mod control;
 mod sh;
 
+fn scanner() -> u32 {
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+   return match trimmed.parse::<u32>() {
+        Ok(i) => i,
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+}
+
 fn main() {
+    println!("-----------------------------------------\n");
+    println!("+              menu                      \n");
+    println!("-----------------------------------------\n");
+    println!(" 1. control statement");
+    println!(" 2. static memory and dynamic memory");
+    let mut number = scanner();
     if number == 1 {
         if given == 1 {
             control::if_statement()
@@ -13,5 +33,5 @@ fn main() {
     }else if number == 2 {
         sh::stack_heap();
     }
-    println!("{}","good to everyone")
+
 }

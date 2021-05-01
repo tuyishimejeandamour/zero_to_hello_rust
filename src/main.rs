04c4620f099189d4fefceec6aa6ba894aa870a1b
damour <tuyishimejeand88@gmail.com>
2021-05-01 08:32:33 +0200

    adding main file

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod control;
+mod sh;
+
+fn main() {
+    if number == 1 {
+        if given == 1 {
+            control::if_statement()
+        } else if given ==2 {
+            control::loop_statement();
+        }else if given == 3 {
+            control::switch_statement();
+        }
+    }else if number == 2 {
+        sh::stack_heap();
+    }
+    println!("{}","good to everyone")
+}
